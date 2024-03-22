241da08cf9221e212559d7429eafc1d5a56e61a6
Gil <38070920+gilberto-009199@users.noreply.github.com>
2024-03-22 02:38:47 -0300

    Update env.php

--- env.php
+++ env.php
@@ -3,12 +3,11 @@
 $_ENV['DB_HOST'] = '127.0.0.1';
 $_ENV['DB_NAME'] = 'reinte33_payment';
 $_ENV['DB_USER'] = 'reinte33_payment_user';
-$_ENV['DB_PASS'] = 'GanheComSorteEGanheBem@!3r22234';
+$_ENV['DB_PASS'] = '';
 
 // Assas Config
 // API KEY PRODUCAO
-//$_ENV['ASSAS_API'] = '$aact_YTU5YTE0M2M2N2I4MTliNzk0YTI5N2U5MzdjNWZmNDQ6OjAwMDAwMDAwMDAwMDAzOTYwMTg6OiRhYWNoXzcwNmY2YzVhLWIzNmItNGQ0Mi1iOTE5LWQzNjZlOThkY2I1YQ==';
-//$_ENV['ASSAS_ENV'] = 'producao';
+
 
 // API KEY HOMOLOGACAO
 $_ENV['ASSAS_API'] = '$aact_YTU5YTE0M2M2N2I4MTliNzk0YTI5N2U5MzdjNWZmNDQ6OjAwMDAwMDAwMDAwMDAwNzQ2MTg6OiRhYWNoXzk1Y2JlNzc2LTMxYjctNGFmZi04MGMxLTM3ZDJkNjRkNjRmMw==';
@@ -26,5 +25,5 @@ $_ENV['PROPHETA_SALE'] = true;
 
 
 // Email commecial
-$_ENV['PROPHETA_MAIL_COMMERCIAL'] = "commercial.propheta@hotmail.com";
-$_ENV['PROPHETA_MAIL_COMMERCIAL_PASS'] = "9hasd897asgd6776ddygbiuwhu798huhn@3e33";
+$_ENV['PROPHETA_MAIL_COMMERCIAL'] = "";
+$_ENV['PROPHETA_MAIL_COMMERCIAL_PASS'] = "";
